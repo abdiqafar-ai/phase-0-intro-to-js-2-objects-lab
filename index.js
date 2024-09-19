1669dab70252a6b9e87b98c698f54b9a7befee03
abdiqafar-ai <iabdiqafar24@gmail.com>
2024-09-19 22:15:47 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const newObj = {...obj};
+
+  newObj[key] = value;
+
+  return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = obj;
+  
+    newObj[key] = value;
+  
+    return newObj;
+}
+function deleteFromEmployeeByKey(employee, obj, key) {
+    const newObj = {...obj};
+  
+    delete newObj.key;
+  
+    return newObj;
+  };
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  
+    return employee;
+  };
